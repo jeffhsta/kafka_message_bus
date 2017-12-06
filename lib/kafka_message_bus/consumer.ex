49ae42b2628f52b6e03fbfedb30f6d294466d351
Jefferson Stachelski <jefferson.stachelski@deepx.it>
2017-12-06 17:02:49 -0200

    Remove some logs about ignoring messages

--- lib/kafka_message_bus/consumer.ex
+++ lib/kafka_message_bus/consumer.ex
@@ -19,12 +19,11 @@ defmodule KafkaMessageBus.Consumer do
 
   defp process_message({:ok, msg_content}, topic) do
     Logger.metadata(request_id: msg_content["request_id"])
-    @processor_config
-    |> Enum.each(&execute_message(msg_content, topic, &1))
+    Enum.each(@processor_config, &execute_message(msg_content, topic, &1))
   end
 
-  defp process_message({:error, error}, topic), do:
-    Logger.error("Failed to parse message in topic #{topic}. Error: #{inspect error}")
+  defp process_message({:error, error}, topic),
+    do: Logger.error("Failed to parse message in topic #{topic}. Error: #{inspect error}")
 
   defp execute_message(msg_content = %{"resource" => resource}, topic, {topic, resource, message_processor}) do
     try do
@@ -34,14 +33,13 @@ defmodule KafkaMessageBus.Consumer do
     end
   end
 
-  defp execute_message(msg_content, _, _), do:
-    Logger.debug fn -> "Ignoring message with no resource: #{inspect msg_content}" end
+  defp execute_message(msg_content, _, _), do: :ok
 
-  defp enqueue_message_retry(msg_content, message_processor, retry_strategy) when retry_strategy == :exq do
-    KafkaMessageBus.ConsumerEnqueuer.enqueue(msg_content, message_processor)
-  end
+  defp enqueue_message_retry(msg_content, message_processor, retry_strategy)
+  when retry_strategy == :exq,
+    do: KafkaMessageBus.ConsumerEnqueuer.enqueue(msg_content, message_processor)
 
-  defp enqueue_message_retry(_msg_content, _message_processor, _retry_strategy), do:
-    Logger.warn("Will not retry message")
+  defp enqueue_message_retry(_msg_content, _message_processor, _retry_strategy),
+    do: Logger.warn("Will not retry message")
 
 end
